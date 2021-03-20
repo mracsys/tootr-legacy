044ed0f428a392898f5ed97de3b21cb739edc388
mracsys <mracsys@gmail.com>
2021-03-20 12:32:30 -0400

    Updates

---
+++ precache-manifest.d5cdeebc098c306d19e59f5c2c0d3a13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8b52826c3916b709af191c2ca5c12a",
+    "url": "/tootr/index.html"
+  },
+  {
+    "revision": "72af2465ff3bf892f261",
+    "url": "/tootr/static/js/2.50663bf5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tootr/static/js/2.50663bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a385800b787cc4e62cde",
+    "url": "/tootr/static/js/main.c2e99614.chunk.js"
+  },
+  {
+    "revision": "d12e5515c3077b068797",
+    "url": "/tootr/static/js/runtime-main.bc7e0cbb.js"
+  }
+]);
