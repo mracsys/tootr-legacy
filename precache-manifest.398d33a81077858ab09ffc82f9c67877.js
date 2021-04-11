4139f0c3abd52d9fb61cba7dabd5329752fa2f0e
mracsys <mracsys@gmail.com>
2021-04-11 00:09:35 -0400

    Updates

--- precache-manifest.398d33a81077858ab09ffc82f9c67877.js
+++ precache-manifest.398d33a81077858ab09ffc82f9c67877.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "61788d9ee1303cf2ba0c46d7374a30cd",
+    "revision": "3dd1dedccdd8452d57ae39e307291991",
     "url": "/tootr/index.html"
   },
   {
-    "revision": "f7f40736818f80d3dbb3",
+    "revision": "0b855347810c08916b61",
     "url": "/tootr/static/css/main.1f7c6a94.chunk.css"
   },
   {
-    "revision": "bc722a813d1e9c5d6e8e",
-    "url": "/tootr/static/js/2.70ee55a7.chunk.js"
+    "revision": "7d9c33dcf05689d920a4",
+    "url": "/tootr/static/js/2.7330141c.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/tootr/static/js/2.70ee55a7.chunk.js.LICENSE.txt"
+    "url": "/tootr/static/js/2.7330141c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f7f40736818f80d3dbb3",
-    "url": "/tootr/static/js/main.27b68096.chunk.js"
+    "revision": "0b855347810c08916b61",
+    "url": "/tootr/static/js/main.34329364.chunk.js"
   },
   {
     "revision": "d12e5515c3077b068797",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tootr/precache-manifest.58428459b9e62170f6607922df7b99ef.js"
+  "/tootr/precache-manifest.398d33a81077858ab09ffc82f9c67877.js"
 );
 
 self.addEventListener('message', (event) => {
